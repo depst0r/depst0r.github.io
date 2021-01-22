095ed45f62bd49306c1aaab984c7ff6bfa1fc267
depst0r <depstor90@gmail.com>
2021-01-22 18:44:41 +0300

    22.01.2021

--- script/script.js
+++ script/script.js
@@ -1,10 +1,20 @@
-const clock = document.querySelector('.clock-number');
-const addZero = num => (num < 10) ? `0${num}` : num;
+window.addEventListener('DOMContentLoaded', () => {
 
-const clockChange = setInterval(() => {
-	const date = new Date();
-	const hours = date.getHours();
-	const minutes = date.getMinutes();
-	const second = date.getSeconds();
-	clock.textContent = `${addZero(hours)}:${addZero(minutes)}:${addZero(second)}`
-}, 1000);
+	function clock() {
+		const clock = document.querySelector('.clock-number');
+
+		const addZero = num => (num < 10) ? `0${num}` : num;
+	
+	 setInterval(() => {
+		const date = new Date();
+		const hours = date.getHours();
+		const minutes = date.getMinutes();
+		const second = date.getSeconds();
+		clock.textContent = `${addZero(hours)}:${addZero(minutes)}:${addZero(second)}`
+	}, 1000);
+};
+
+
+
+	clock();
+});
