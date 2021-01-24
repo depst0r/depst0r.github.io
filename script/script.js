e2a6aad7337f7ebcdf9f2ee56aae1f46de300813
depst0r <depstor90@gmail.com>
2021-01-24 11:42:53 +0300

    new-header

--- script/script.js
+++ script/script.js
@@ -1,20 +1,20 @@
-window.addEventListener('DOMContentLoaded', () => {
+// window.addEventListener('DOMContentLoaded', () => {
 
-	function clock() {
-		const clock = document.querySelector('.clock-number');
+// 	function clock() {
+// 		const clock = document.querySelector('.clock-number');
 
-		const addZero = num => (num < 10) ? `0${num}` : num;
+// 		const addZero = num => (num < 10) ? `0${num}` : num;
 	
-	 setInterval(() => {
-		const date = new Date();
-		const hours = date.getHours();
-		const minutes = date.getMinutes();
-		const second = date.getSeconds();
-		clock.textContent = `${addZero(hours)}:${addZero(minutes)}:${addZero(second)}`
-	}, 1000);
-};
+// 	 setInterval(() => {
+// 		const date = new Date();
+// 		const hours = date.getHours();
+// 		const minutes = date.getMinutes();
+// 		const second = date.getSeconds();
+// 		clock.textContent = `${addZero(hours)}:${addZero(minutes)}:${addZero(second)}`
+// 	}, 1000);
+// };
 
 
 
-	clock();
-});
+// 	clock();
+// });
