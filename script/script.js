3b287824935bb525cc9e9486121dbabb74231374
depst0r <depstor90@gmail.com>
2021-01-26 11:28:27 +0300

    test-bootstrap

--- script/script.js
+++ script/script.js
@@ -1,27 +1,30 @@
 window.addEventListener('DOMContentLoaded', () => {
 
-// 	function clock() {
-// 		const clock = document.querySelector('.clock-number');
+	function clock() {
+		const clock = document.querySelector('.clock-number');
 
-// 		const addZero = num => (num < 10) ? `0${num}` : num;
+		const addZero = num => (num < 10) ? `0${num}` : num;
 	
-// 	 setInterval(() => {
-// 		const date = new Date();
-// 		const hours = date.getHours();
-// 		const minutes = date.getMinutes();
-// 		const second = date.getSeconds();
-// 		clock.textContent = `${addZero(hours)}:${addZero(minutes)}:${addZero(second)}`
-// 	}, 1000);
-// };
+	 setInterval(() => {
+		const date = new Date();
+		const hours = date.getHours();
+		const minutes = date.getMinutes();
+		const second = date.getSeconds();
+		clock.textContent = `${addZero(hours)}:${addZero(minutes)}:${addZero(second)}`
+	}, 1000);
+};
 
 function handlerHoverColor() {
     const iconContact = document.querySelector('.main-block-contact');
+    
 
     iconContact.onmouseover = event => {
         event.target.classList.toggle('black_and_white')
     }
 
+
+
 }
     handlerHoverColor();
-	// clock();
+	clock();
 });
